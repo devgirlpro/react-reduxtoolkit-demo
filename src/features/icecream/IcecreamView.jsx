1894311f34c488880fa942778d6e7903a6e64ffc
devgirlpro <agalidari@gmail.com>
2024-03-01 17:39:53 +0100

    use useSelector in icecreamView to get access to the state.icecram.numberOfIcecreams and display it

--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export const IcecreamView = () => {
+  const numberOfIcecreams = useSelector((state) => state.icecream.numberOfIcecreams)
   return (
     <div>
-        <h2>Number Of IceCream</h2>
+        <h2>Number Of IceCreams - {numberOfIcecreams}</h2>
         <button>Order IceCream</button>
         <button>Restocked Icecreams</button>
     </div>
