91b0cb5ff7118eaffd3c967625ed5ae91f364d61
devgirlpro <agalidari@gmail.com>
2024-03-01 18:42:15 +0100

    the value of input elements has not to be the part of redux store by example

--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { ordered, restocked } from './icecreamSlice'
 
 export const IcecreamView = () => {
+  const [inputValue, setInputValue] = useState(1)
   const numberOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams)
   const dispatch = useDispatch()
   return (
     <div>
         <h2>Number Of IceCreams - {numberOfIcecreams}</h2>
         <button onClick={() => dispatch(ordered())} >Order IceCream</button>
-        <button onClick={() => dispatch(restocked(4))} >Restocked Icecreams</button>
+        <input 
+          type="number" 
+          value= {inputValue} 
+          onChange={(e) => setInputValue(parseInt(e.target.value))} 
+        />
+        <button onClick={() => dispatch(restocked(inputValue))} >Restocked Icecreams</button>
     </div>
   )
 }
