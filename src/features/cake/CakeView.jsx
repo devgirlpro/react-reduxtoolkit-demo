94e6d14d6d2162bc01b85999b28e1578117ae32d
devgirlpro <agalidari@gmail.com>
2024-03-01 17:30:09 +0100

    using useSelector hooks to get hold of any states is maintained in the redux- store

--- src/features/cake/CakeView.jsx
+++ src/features/cake/CakeView.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export const CakeView = () => {
+ const numberOfCakes =  useSelector((state) => state.cake.numberOfCakes)
   return (
     <div>
-        <h2>Number Of Cakes -</h2>
+        <h2>Number Of Cakes -{numberOfCakes} </h2>
         <button>Order Cake</button>
         <button>Restore Cakes</button>
     </div>
